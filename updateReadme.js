79cac866b824a4faeaf8daa46a3a96c2affc2ebe
Andrew Hickman <andrew.hickman.dev@gmail.com>
2024-03-02 12:09:09 -0500

    Add publish date to recent posts

--- updateReadme.js
+++ updateReadme.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const Parser = require('rss-parser');
+const { format } = require('date-fns');
 const parser = new Parser();
 
 const README_FILE = 'README.md';
@@ -8,7 +9,10 @@ const POSTS_LIMIT = 4;
 
 (async () => {
     let feed = await parser.parseURL(FEED_URL);
-    let latestPosts = feed.items.slice(0, POSTS_LIMIT).map(item => `- [${item.title}](${item.link})`).join('\n');
+    let latestPosts = feed.items.slice(0, POSTS_LIMIT).map(item => {
+        let formattedDate = format(new Date(item.pubDate), 'MMMM do, yyyy');
+        return `- [${item.title}](${item.link}) - *${formattedDate}*`;
+    }).join('\n');
 
     let readmeContent = fs.readFileSync(README_FILE, 'utf8');
     let updatedReadme = readmeContent.replace(/(<!-- BLOGPOSTS:START -->)[\s\S]*(<!-- BLOGPOSTS:END -->)/, `$1\n${latestPosts}\n$2`);
